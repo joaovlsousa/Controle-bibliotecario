2e74857ae7784507cb464edbd7b79645459f8b85
Joao Vitor <joaov.lmsousa@gmail.com>
2023-09-18 18:43:09 -0300

    feat: migrando a estrutura de dados para lista encadeada

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-struct Livro
-{
+// ESTRUTURAS
+
+struct Livro {
     string titulo;
     string autor;
     float preco;
     int quant;
 };
 
-// funções utilitárias
+struct Acervo {
+    Livro livro;
+    Acervo *prox;
+};
 
-void mostrar_livro(Livro livro)
-{
+//  FUNÇÕES UTILITÁRIAS
+
+Acervo * criar_Acervo() {
+    return NULL;
+}
+
+void imprimir_livro(Livro livro) {
     cout << endl << "-------------------------------------------" << endl;
     cout << "Título: " << livro.titulo << endl;
     cout << "Autor: " << livro.autor << endl;
@@ -23,26 +31,25 @@ void mostrar_livro(Livro livro)
     cout << "-------------------------------------------" << endl;
 }
 
-int livro_existe(vector<Livro> &acervo, string titulo)
-{
-    int tam = acervo.size();
-    int index = -1;
+Livro livro_existe(Acervo *estoque, string titulo) {
+    Acervo *pos = estoque;
+    
+    do {
+        if (pos->livro.titulo == titulo) 
+            return pos->livro;
+        else 
+            pos = pos->prox; 
+        
+    } while (pos != NULL);
+    
+    Livro livro_inexistente = { .quant = -1 };
 
-    for(int i = 0; i < tam; i++)
-    {
-        if(acervo[i].titulo == titulo)
-        {
-            index = i;
-        }
-    }
-
-    return index;
+    return livro_inexistente;
 }
 
-// funções 
+//FUNÇÕES
 
-void menu()
-{
+void menu() {
     cout << endl << "---------- Sistema Bibliotecário ----------" << endl;
     cout << "1 - Cadastrar um novo livro" << endl;
     cout << "2 - Listar todos os livros" << endl;
@@ -53,102 +60,125 @@ void menu()
     cout << "-------------------------------------------" << endl;
 }
 
-void cadastrar_livro(vector<Livro> &acervo)
-{
+
+void anexar_livro(Acervo *&estoque, Livro livro) {
+    Acervo *novo_livro = new Acervo;
+    novo_livro->livro = livro;
+    novo_livro->prox = NULL;
+    
+    if (estoque == NULL) 
+        estoque = novo_livro;
+
+    else {
+        Acervo *pos = estoque;
+
+        while(pos->prox != NULL) 
+            pos = pos->prox;
+
+        pos->prox = novo_livro;     
+    }
+}
+
+
+void cadastrar_livro(Acervo *&estoque) {
     Livro novo_livro;
 
-    cout << endl << "Cadastrar Livro" << endl;
+    cout << "Cadastrar livro" << endl;
 
-    cout << "Informe o título do livro: ";
+    cout<<"Informe o titulo do livro: ";
     cin >> novo_livro.titulo;
-    
-    cout << "Informe o autor do livro: ";
+
+    cout << "Informe o nome do autor: ";
     cin >> novo_livro.autor;
-    
+
     cout << "Informe o preço do livro: ";
     cin >> novo_livro.preco;
-    
+
     cout << "Informe a quantidade de livros disponíveis: ";
     cin >> novo_livro.quant;
 
-    acervo.push_back(novo_livro);
+    anexar_livro(estoque, novo_livro);
 
-    cout << endl << "Livro cadastrado com sucesso!" << endl;
+    cout << endl << "Livro cadastrado com sucesso." << endl;
+
 }
 
-void listar_livros(vector<Livro> &acervo)
-{
-    int tam = acervo.size();
 
-    for(int i = 0; i < tam; i++)
-    {
-        mostrar_livro(acervo[i]);
+void listar_livros(Acervo *estoque) {
+    if (estoque == NULL)
+        cout << endl << "Nenhum Livro cadastrado." << endl;
+
+    else {
+        Acervo *pos = estoque;
+
+        while (pos != NULL) {
+            imprimir_livro(pos->livro);
+
+            pos = pos -> prox;
+        }    
     }
 }
 
-void buscar_livro(vector<Livro> &acervo, string titulo)
-{
-    int index = livro_existe(acervo, titulo);
 
-    if(index < 0)
-    {
-        cout << "O livro " << titulo << " não está no nosso acervo." << endl;
-    }
+void buscar_livro(Acervo *estoque, string titulo) {
+    Livro checar_Livro = livro_existe(estoque, titulo);
+
+    if(checar_Livro.quant < 0)
+        cout << endl << "O livro não existe no estoque." << endl;
+
     else
-    {
-        mostrar_livro(acervo[index]);
-    }
+        imprimir_livro(checar_Livro); 
 }
 
-void editar_livro(vector<Livro>, string)
-{}
+void editar_livro() {
 
-void excluir_livro(vector<Livro>, string)
-{}
+}
+
+void excluir_livro() {
+
+}
 
 
-int main()
-{
+int main() {
+    Acervo *estoque = criar_Acervo();
     int op;
     string titulo;
-    vector<Livro> acervo;
 
     cout << endl << "Bem-vindo ao controle bibliotecário" << endl;
 
-    do
-    {
+    do {
         menu();
         cin >> op;
 
         switch (op)
         {
         case 1:
-            cadastrar_livro(acervo);
+            cout << endl;
+            cadastrar_livro(estoque);
             break;
 
         case 2:
-            listar_livros(acervo);
+            cout << endl;
+            listar_livros(estoque);
             break;
 
         case 3:
-            cout << "Por favor, informe o titulo do livro: ";
+            cout << endl << "Por favor, informe o titulo do livro: ";
             cin >> titulo;
-
-            buscar_livro(acervo, titulo);
+            buscar_livro(estoque, titulo);
+            
             break;
 
         case 4:
-            cout << "Por favor, informe o titulo do livro: ";
+            cout << endl << "Por favor, informe o titulo do livro: ";
             cin >> titulo;
-
-            editar_livro(acervo, titulo);
+        
             break;
 
         case 5:
-            cout << "Por favor, informe o titulo do livro: ";
+            cout << endl << "Por favor, informe o titulo do livro: ";
             cin >> titulo;
 
-            excluir_livro(acervo, titulo);
             break;
         }
 
